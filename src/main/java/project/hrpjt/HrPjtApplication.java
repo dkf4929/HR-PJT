e6ae9e75e8173b8f52a89853d8b34169d56bb450
dkf4929 <sng4929@naver.com>
2022-11-29 22:57:54 +0900

    oauth backup

---
+++ src/main/java/project/hrpjt/HrPjtApplication.java
@@ -0,0 +1,13 @@
+package project.hrpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrPjtApplication.class, args);
+	}
+
+}
