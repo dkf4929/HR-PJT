ff14fb86899644b1d01e657ac81d7188da6e0ba4
SongYeongho <dkf4928@naver.com>
2022-12-09 22:44:19 +0900

    조직도 수정

--- src/main/java/project/hrpjt/organization/dto/OrganizationFindDto.java
+++ src/main/java/project/hrpjt/organization/dto/OrganizationFindDto.java
@@ -1,27 +1,26 @@
 package project.hrpjt.organization.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import project.hrpjt.organization.entity.Organization;
 
 import java.time.LocalDate;
-import java.util.*;
+import java.util.List;
 import java.util.stream.Collectors;
 
 @NoArgsConstructor
 @Getter
-@ToString(of = {"orgNo", "orgNm"})
 public class OrganizationFindDto {
     private String orgNo;
     private String orgNm;
-    private List<OrganizationFindDto> child = new ArrayList<>();
-
+    private List<OrganizationFindDto> child;
 
     @Builder
-    public OrganizationFindDto(Organization organization, Set<Organization> childs) {
+    public OrganizationFindDto(Organization organization) {
         this.orgNo = organization.getOrgNo();
         this.orgNm = organization.getOrgNm();
-        for (Organization child : childs) {
-            this.child.add(OrganizationFindDto.builder().childs(child.getChildren()).organization(child).build());
-        }
+        this.child = organization.getChildren().stream().map(OrganizationFindDto::new).collect(Collectors.toList());
     }
 }
