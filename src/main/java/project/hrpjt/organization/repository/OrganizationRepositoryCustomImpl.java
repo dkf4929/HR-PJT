b9f9d46a9b06f19f23cad31caad0138b1956e0b0
SongYeongho <dkf4928@naver.com>
2022-12-10 13:54:11 +0900

    조직도 수정(같은 조직이 두번씩 나옴.)

--- src/main/java/project/hrpjt/organization/repository/OrganizationRepositoryCustomImpl.java
+++ src/main/java/project/hrpjt/organization/repository/OrganizationRepositoryCustomImpl.java
@@ -18,6 +18,7 @@ import java.io.UnsupportedEncodingException;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 import static project.hrpjt.employee.entity.QEmployee.*;
@@ -34,7 +35,7 @@ public class OrganizationRepositoryCustomImpl implements OrganizationRepositoryC
     }
 
     @Override
-    public List<Organization> findAllOrg(OrganizationFindParamDto dto) {
+    public List<OrganizationFindDto> findAllOrg(OrganizationFindParamDto dto) {
         QOrganization sub = new QOrganization("sub");
         QOrganization children = new QOrganization("children");
 
@@ -46,21 +47,29 @@ public class OrganizationRepositoryCustomImpl implements OrganizationRepositoryC
 //                .from(organization).leftJoin(organization.parent, parent).fetchJoin()
                 .from(organization).leftJoin(organization.children, children).fetchJoin()
 //                .where(orgNmContain(dto.getOrgNm()), orgNoEq(dto.getOrgNo()))
-                .where(organization.parent.isNotNull())
+                .orderBy(organization.orgNo.asc().nullsFirst())
                 .fetch();
 
-        fetch.stream().forEach(c -> System.out.println("organization : " + c.get(organization) + " parent : " + c.get(organization.parent)));
 
-        Map<Organization, List<Tuple>> collect = fetch.stream()
-                .collect(Collectors.groupingBy(tuple -> tuple.get(organization.parent)));
+        Map<Organization, List<Tuple>> collect = fetch.stream().collect(Collectors.groupingBy(tuple -> tuple.get(organization.parent)));
 
+//        return collect.entrySet().stream()
+//                .distinct()
+//                .map(entry -> OrganizationFindDto.builder()
+//                        .childs(entry.getValue().stream().distinct()
+//                                .map(tuple -> tuple.get(organization)).collect(Collectors.toSet()))
+//                        .organization(entry.getKey())
+//                        .build())
+//                .collect(Collectors.toList());
 
-        collect.entrySet().stream()
-                .forEach(
-                        e -> System.out.println("parent : " + e.getKey() + " child : " + collect.get((e.getKey())))
-                );
-
-        return null;
+        return collect.keySet().stream()
+                .distinct()
+                .map(entry -> OrganizationFindDto.builder()
+                        .childs(collect.get(entry).stream().distinct()
+                                .map(c -> c.get(organization)).collect(Collectors.toSet()))
+                        .organization(entry)
+                        .build())
+                .collect(Collectors.toList());
     }
 
     @Override
