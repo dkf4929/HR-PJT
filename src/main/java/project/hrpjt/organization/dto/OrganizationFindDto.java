d4f83c80c7bb3c069fc7da50229cd1dd6f0bb46f
dkf4929 <sng4929@naver.com>
2022-12-12 22:46:17 +0900

    조직도 수정

--- src/main/java/project/hrpjt/organization/dto/OrganizationFindDto.java
+++ src/main/java/project/hrpjt/organization/dto/OrganizationFindDto.java
@@ -1,27 +1,21 @@
 package project.hrpjt.organization.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.querydsl.core.annotations.QueryProjection;
 import lombok.*;
-import org.springframework.stereotype.Component;
 import project.hrpjt.organization.entity.Organization;
-
-import java.time.LocalDate;
 import java.util.*;
-import java.util.stream.Collectors;
 
 @NoArgsConstructor
-@Component
 @Getter
-@EqualsAndHashCode(of = {"orgNo", "orgNm"})
 @ToString(of = {"orgNo", "orgNm"})
+@EqualsAndHashCode(of = {"orgNo", "orgNm"})
 public class OrganizationFindDto {
     private String orgNo;
     private String orgNm;
     private Set<OrganizationFindDto> child = new HashSet<>();
 
     @JsonIgnore
-    private OrganizationFindDto parent;
+    private static OrganizationFindDto parent;
 
     @Builder
     public OrganizationFindDto(Organization organization, Set<Organization> childs, OrganizationFindDto parent) {
@@ -33,25 +27,22 @@ public class OrganizationFindDto {
             organizationFindDto.setOrgNo(org.getOrgNo());
             organizationFindDto.setOrgNm(org.getOrgNm());
 
-            if (this.parent == null) {
+            if (OrganizationFindDto.parent == null) {
                 this.getChild().add(organizationFindDto);
             } else {
-                this.parent.getChild().add(organizationFindDto);
+                OrganizationFindDto.parent.getChild().add(organizationFindDto);
             }
+
+
+            OrganizationFindDto.parent = this;
+
+            System.out.println("parent : " + OrganizationFindDto.parent);
+            System.out.println("child : " + getChild());
         }
-
-        this.parent = this;
-
-        System.out.println("parent = " + this.hashCode() + " child : " + this.getChild().hashCode());
-        System.out.println("parent = " + this + " child : " + this.getChild());
     }
 
     public void setOrgNo(String orgNo) {
         this.orgNo = orgNo;
-    }
-
-    public void setParent(OrganizationFindDto parent) {
-        this.parent = parent;
     }
 
     public void setOrgNm(String orgNm) {
