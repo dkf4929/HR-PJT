46a81c5878f25c4438e3e9f2e7d58e46718e6b28
SongYeongho <dkf4928@naver.com>
2022-12-22 15:34:33 +0900

    테스트 코드 일부 수정

--- src/test/java/project/hrpjt/service/AppointmentServiceTest.java
+++ src/test/java/project/hrpjt/service/AppointmentServiceTest.java
@@ -1,12 +1,5 @@
 package project.hrpjt.service;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.gson.JsonParser;
-import org.assertj.core.api.Assertions;
-import org.json.simple.JSONObject;
-import org.json.simple.parser.JSONParser;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -30,12 +24,10 @@ import project.hrpjt.appointment.entity.enumeration.AppointmentType;
 import project.hrpjt.appointment.repository.AppointmentRepository;
 import project.hrpjt.appointment.service.AppointmentService;
 
+import javax.persistence.EntityManager;
 import javax.servlet.http.Cookie;
-import java.nio.charset.Charset;
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.*;
@@ -46,46 +38,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 @Transactional
 public class AppointmentServiceTest {
-    @Autowired MockMvc mockMvc;
-    @Autowired ObjectMapper objectMapper;
     @Autowired AppointmentService appointmentService;
     @Autowired AppointmentRepository appointmentRepository;
     @Autowired UserDetailsService userDetailsService;
-
-    AtomicReference<String> cookieValue = new AtomicReference<>("");
+    @Autowired EntityManager entityManager;
 
     @BeforeEach
     void each() throws Exception {
-        mockMvc.perform(post("/login")
-                        .param("empNo", "ADMIN")  // admin 권한으로 로그인
-                        .param("password", "1234")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(result -> Arrays.stream(result.getResponse().getCookies())
-                        .filter((c) -> c.getName().equals("jwtToken"))
-                        .forEach((c) -> cookieValue.set(c.getValue())));   // result에서 쿠키값 추출
-    }
+        UserDetails userDetails = userDetailsService.loadUserByUsername("ADMIN");
+        userDetails.getAuthorities();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 
-    @Test
-    @DisplayName("내 발령 이력 조회")
-    void myAppointment() throws Exception {
-        mockMvc.perform(get("/role_emp/appointment")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .cookie(new Cookie("jwtToken", cookieValue.get())))
-                .andExpect(status().isOk())
-                .andDo(result -> System.out.println("result = " + result.getResponse().getContentAsString(Charset.forName("UTF-8"))));
+        SecurityContextHolder.getContext().setAuthentication(authentication);
     }
 
     @Test
     @DisplayName("발령 history 조회")
     void appointmentList() throws Exception {
-        mockMvc.perform(get("/role_lead/appointment")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .cookie(new Cookie("jwtToken", cookieValue.get())))
-                .andExpect(status().isOk())
-                .andDo(result -> System.out.println("result = " + result.getResponse().getContentAsString(Charset.forName("UTF-8"))));
+        Page<AppointmentFindDto> all = appointmentService.findAllByParam(null, null, PageRequest.of(1, 50));
+
+        System.out.println("size = " + all.getContent().size());
     }
 
     // 1. 부서원 발령을 등록한다.
@@ -103,80 +75,80 @@ public class AppointmentServiceTest {
 
         AppointmentFindDto savedDto = appointmentService.save(dto);
 
-        mockMvc.perform(post("/login")
-                        .param("empNo", "ORG_LEADER")  // 인사부 조직장으로 로그인.
-                        .param("password", "1234")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(result -> Arrays.stream(result.getResponse().getCookies())
-                        .filter((c) -> c.getName().equals("jwtToken"))
-                        .forEach((c) -> cookieValue.set(c.getValue())));
+        // 인사부 조직장으로 로그인 했다고 가정
+        UserDetails userDetails = userDetailsService.loadUserByUsername("ORG_LEADER");
+        userDetails.getAuthorities();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 
-        mockMvc.perform(get("/role_lead/approve")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .cookie(new Cookie("jwtToken", cookieValue.get())))
-                .andExpect(status().isOk())
-                .andDo(result -> {
-                    String contents = result.getResponse().getContentAsString(Charset.forName("UTF-8"));  // 결과를 string으로 받아온다.
-                    JSONParser jsonParser = new JSONParser();
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-                    JSONObject obj = (JSONObject) jsonParser.parse(contents);  // json 파싱
-                    String content = String.valueOf(obj.get("content"));
+        Page<AppointmentFindDto> all = appointmentService.findAllApprList(PageRequest.of(1, 50));
 
-                    objectMapper = objectMapper.enable(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY);
-                    objectMapper.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT);
-
-                    Object[] objects = objectMapper.readValue(content, Object[].class);
-                    List<AppointmentFindDto> list = objectMapper.convertValue(objects, new TypeReference<List<AppointmentFindDto>>() {});  // api 결과를 dto로 변환
-
-                    assertThat(list).contains(savedDto);  // 등록한 발령이 리스트에 있는지 확인.
-                });
+        assertThat(all.getContent()).contains(savedDto);
     }
 
-//  1. 조직장이 조직장 하위 조직 발령 또는 부서원의 발령을 승인한다.
+    //  1. 조직장이 조직장 하위 조직 발령 또는 부서원의 발령을 승인한다.
 //  2. 상태가 CEO_PENDING_APPR(CEO 승인 대기)로 업데이트
 //  3. CEO 또는 시스템 관리자가 승인한다.
 //  4. 상태가 APPR(최종 승인) 상태로 변경되고 발령 구분에 따라 처리 로직을 수행한다.
     @Test
     @DisplayName("발령 승인")
     void approve() throws Exception {
-        mockMvc.perform(post("/login")
-                        .param("empNo", "LEADER2")  // 총무부 인사이동 발령 승인을 위해 총무부 부서장으로 로그인.
-                        .param("password", "1234")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(result -> Arrays.stream(result.getResponse().getCookies())
-                        .filter((c) -> c.getName().equals("jwtToken"))
-                        .forEach((c) -> cookieValue.set(c.getValue())));   // result에서 쿠키값 추출
+        // 총무부 인사이동 발령 승인을 위해 총무부 부서장으로 로그인했다고 가정.
+        UserDetails userDetails = userDetailsService.loadUserByUsername("LEADER2");
+        userDetails.getAuthorities();
+        Authentication authentication = new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
 
-        mockMvc.perform(put("/role_lead/approve/23")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .cookie(new Cookie("jwtToken", cookieValue.get())))
-                .andExpect(status().isOk())
-                .andDo(result -> {
-                    Appointment appointment = appointmentRepository.findById(23L).get();
-                    assertThat(appointment.getAppointmentStatus()).isEqualTo(AppointmentStatus.CEO_PENDING_APPR);
-                });
+        SecurityContextHolder.getContext().setAuthentication(authentication);
 
-        mockMvc.perform(post("/login")
-                        .param("empNo", "CEO")  // CEO로 로그인.
-                        .param("password", "1234")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON))
-                .andDo(result -> Arrays.stream(result.getResponse().getCookies())
-                        .filter((c) -> c.getName().equals("jwtToken"))
-                        .forEach((c) -> cookieValue.set(c.getValue())));   // result에서 쿠키값 추출
+        // 조직장이 총무팀 조직 이동 발령 승인
+        appointmentService.approve(23L, PageRequest.of(1, 10));
 
-        mockMvc.perform(put("/role_lead/approve/23")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .accept(MediaType.APPLICATION_JSON)
-                        .cookie(new Cookie("jwtToken", cookieValue.get())))
-                .andExpect(status().isOk())
-                .andDo(result -> {
-                    Appointment appointment = appointmentRepository.findById(23L).get();
-                    assertThat(appointment.getAppointmentStatus()).isEqualTo(AppointmentStatus.APPR);
-                });
+        Appointment appointment = appointmentRepository.findById(23L).get();
+        assertThat(appointment.getAppointmentStatus()).isEqualTo(AppointmentStatus.CEO_PENDING_APPR); // CEO 승인대기 상태로 변경.
+
+        // 시스템 관리자 권한으로 로그인
+        each();
+
+        appointmentService.approve(23L, PageRequest.of(1, 10)); // 시스템 관리자 조직 이동 발령 승인
+
+        Appointment appr = appointmentRepository.findById(23L).get();
+        assertThat(appr.getAppointmentStatus()).isEqualTo(AppointmentStatus.APPR); // 최종승인 상태로 변경.
+
+        assertThat(appr.getTransOrg()).isEqualTo(appr.getEmployee().getOrganization()); // 최종 승인된 조직과 직원 엔터티의 조직이 같은지 확인.
+        assertThat(appr.getTransOrg().getEmployees()).contains(appr.getEmployee());  // 조직 엔터티에 발령된 직원이 포함되어 있는지 확인.
+
+        // 인사팀으로 새로운 조직 발령.
+        AppointmentSaveDto dto = AppointmentSaveDto.builder()
+                .startDate(LocalDate.of(2023, 1, 1))
+                .orgNo("000030")
+                .empNo("EMPLOYEE2")
+                .type(AppointmentType.ORG)
+                .build();
+
+        AppointmentFindDto save = appointmentService.save(dto);
+
+        System.out.println("save = " + save);
+
+        UserDetails orgLeader = userDetailsService.loadUserByUsername("ORG_LEADER");
+        orgLeader.getAuthorities();
+        Authentication auth = new UsernamePasswordAuthenticationToken(orgLeader, "", orgLeader.getAuthorities());
+
+        SecurityContextHolder.getContext().setAuthentication(auth);
+
+        appointmentService.approve(save.getAppointmentId(), PageRequest.of(1, 50));
+
+        each(); // admin으로 로그인.
+
+        Appointment latestApp = appointmentRepository.findLatestApp(appr.getEmployee().getId()).get();
+
+        Page<AppointmentFindDto> approve = appointmentService.approve(save.getAppointmentId(), PageRequest.of(1, 50));
+
+        entityManager.flush();
+        entityManager.clear();
+
+        Appointment appr2 = appointmentRepository.findById(save.getAppointmentId()).get();
+
+        assertThat(latestApp.getEndDate()).isEqualTo(appr2.getStartDate().minusDays(1)); // 이전 조직발령 종료일 = 현 조직 발령 시작일 - 1일
     }
 }
