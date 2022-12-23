dfa85371944121f13033702c4905e3cfb9b79e2c
dkf4929 <sng4929@naver.com>
2022-12-23 22:19:39 +0900

    연차 감산 로직 추가(수정 필요)

---
+++ src/main/java/project/hrpjt/dayoff/repository/DayOffRepositoryCustom.java
@@ -0,0 +1,13 @@
+package project.hrpjt.dayoff.repository;
+
+import project.hrpjt.dayoff.entity.DayOff;
+import project.hrpjt.employee.entity.Employee;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DayOffRepositoryCustom {
+    public List<DayOff> findMyDayOff(Employee employee, List<Integer> years);
+
+    public Optional<DayOff> findDayOff(Employee employee, int year);
+}
