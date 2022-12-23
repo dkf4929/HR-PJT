2c3962eaf670ac8f76b443e6417f38d24a6a0865
SongYeongho <dkf4928@naver.com>
2022-12-23 22:19:39 +0900

    연차 감산 로직 추가(수정 필요)

---
+++ src/main/java/project/hrpjt/dayoff/repository/DayOffRepositoryCustomImpl.java
@@ -0,0 +1,40 @@
+package project.hrpjt.dayoff.repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import project.hrpjt.dayoff.entity.DayOff;
+import project.hrpjt.dayoff.entity.QDayOff;
+import project.hrpjt.employee.entity.Employee;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+import static project.hrpjt.dayoff.entity.QDayOff.*;
+
+public class DayOffRepositoryCustomImpl implements DayOffRepositoryCustom {
+    private final EntityManager entityManager;
+    private final JPAQueryFactory queryFactory;
+
+    public DayOffRepositoryCustomImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+        this.queryFactory = new JPAQueryFactory(entityManager);
+    }
+
+    @Override
+    public List<DayOff> findMyDayOff(Employee employee, List<Integer> years) {
+        return queryFactory
+                .selectFrom(dayOff)
+                .where(dayOff.employee.eq(employee).and(dayOff.year.in(years)))
+                .fetch();
+    }
+
+    @Override
+    public Optional<DayOff> findDayOff(Employee employee, int year) {
+        return Optional.ofNullable(
+                queryFactory
+                        .selectFrom(dayOff)
+                        .where(dayOff.employee.eq(employee).and(dayOff.year.eq(year)))
+                        .fetchOne()
+        );
+    }
+}
