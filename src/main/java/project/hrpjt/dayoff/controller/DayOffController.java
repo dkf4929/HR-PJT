6f0878d27e47bb3d87f2d64364f72b4205d19842
SongYeongho <dkf4928@naver.com>
2022-12-27 15:43:08 +0900

    연차사용촉진 대상자 메일발송 기능 추가

---
+++ src/main/java/project/hrpjt/dayoff/controller/DayOffController.java
@@ -0,0 +1,24 @@
+package project.hrpjt.dayoff.controller;
+
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import project.hrpjt.dayoff.service.DayOffService;
+
+@RestController
+@RequiredArgsConstructor
+public class DayOffController {
+    private final DayOffService dayOffService;
+
+    @ApiOperation(
+            value = "연차촉진 메일 발송",
+            notes = "현재년도 기준 잔여연차가 10일 이상 남은 직원들에게 연차사용 촉진 메일 발송"
+    )
+    @PostMapping("/role_adm/day_off/mail")
+    public String sendMail() {
+        dayOffService.sendMail();
+
+        return "메일 발송 완료";
+    }
+}
